63ebbfecd0c950c5ed76ff2f03a21d7dae94ca4e
Jeb Smithwick <jsmithwick@gannett.com>
2015-01-11 14:45:02 -0500

    added additional scenarios

--- features/feature1.feature
+++ features/feature1.feature
@@ -8,7 +8,21 @@ Scenario: Check the Dominos home page
   Then I should see the Dominos home page
 @location
 Scenario: Location Search for Dominos Pizza
-   Given I visit the Dominos home page
-   When I click the "Order Online" tab
-   Then I should see the Dominos Location Search page
+  Given I visit the Dominos home page
+  When I click the "Order Online" tab
+  Then I should see the Dominos Location Search page
+@location
+Scenario: Complete Location Search
+  And I select "Business" from address type drop down
+  And I enter “130 S. Meridian” in the street address field
+  And I enter “Indianapolis” in the city field
+  And I select “IN from state drop down
+  And I click the “Continue” button
+  Then I should see “130 S. Meridian” selected in the order settings
+@orderselection
+Scenario: Select Order Items
+  Then I expand “Specialty Pizza”
+  Then I click “Order” for Chicken & Bacon Carbonara
+  Then I click “Add to Order”
+  Then I click “Checkout” button
 
--- features/step_definitions/feature1_steps.rb
+++ features/step_definitions/feature1_steps.rb
@@ -6,7 +6,53 @@ Then /^I should see the Dominos home page$/ do
 end
 When(/^I click the "(.*?)" tab$/) do |tab|
     click_link('Order Online')
+    find('.Delivery').click
 end
 Then (/^I should see the Dominos Location Search page$/) do
     expect(page).to have_selector(:id, 'locationsSearchPage')
+end
+def fill_out_location
+    
+    select('Business', from: 'Address_Type_Select')
+    fill_in('Street', with: '130 S. Meridian')
+    fill_in('City', with: 'Indianapolis')
+    select('IN', from: 'Region')
+    fill_in('Postal_Code', :with => '46225')
+end
+Then(/^I select "(.*?)" from address type drop down$/) do |arg1|
+    # express the regexp above with the code you wish you had
+end
+
+Then(/^I enter “(\d+) S\. Meridian” in the street address field$/) do |arg1|
+    # express the regexp above with the code you wish you had
+end
+
+Then(/^I enter “Indianapolis” in the city field$/) do
+    # express the regexp above with the code you wish you had
+end
+
+Then(/^I select “IN from state drop down$/) do
+    # express the regexp above with the code you wish you had
+end
+
+def continue_button
+    find_button('Continue').click
+end
+Then(/^I click the “Continue” button$/) do
+    # express the regexp above with the code you wish you had
+end
+Then(/^I should see “(\d+) S\. Meridian” selected in the order settings$/) do |arg1|
+    # express the regexp above with the code you wish you had
+end
+Then(/^I expand “Specialty Pizza”$/) do
+    # express the regexp above with the code you wish you had
+end
+Then(/^I click “Order” for Chicken & Bacon Carbonara$/) do
+    # express the regexp above with the code you wish you had
+end
+Then(/^I click “Add to Order”$/) do
+    # express the regexp above with the code you wish you had
+end
+Then(/^I click “Checkout” button$/) do
+    # express the regexp above with the code you wish you had
 end
